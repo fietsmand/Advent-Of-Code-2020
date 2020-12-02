1082bb8eee6840b4012fb4759739c7da0b2be652
Daniel Huizenga <djd.huizenga@gmail.com>
2020-12-02 15:06:08 +0100

    Day 2 second puzzle complete

--- day-2/index.ts
+++ day-2/index.ts
@@ -3,8 +3,6 @@ import input from './input';
 import firstPuzzle from './puzzle-1';
 import secondPuzzle from './puzzle-2';
 
-
 console.log('Output of the first puzzle is: ', firstPuzzle(input))
 
-
-console.log('Output of the second puzzle is: ', secondPuzzle())
+console.log('Output of the second puzzle is: ', secondPuzzle(input))
--- day-2/puzzle-2/index.ts
+++ day-2/puzzle-2/index.ts
@@ -1,7 +1,33 @@
-const secondPuzzle = (
-
-) => {
-
+interface Input {
+  [passwordPolicy: string]: string;
 }
 
-export default secondPuzzle;
+const firstPuzzle = (
+  input: Input[]
+): number => {
+  let validPasswords: number = 0;
+
+  input.map(passwordObject => {
+    const passwordPolicy = Object.keys(passwordObject)[0];
+    const password = passwordObject[passwordPolicy];
+
+    const [positions, passwordShouldContain] = passwordPolicy.split(' ');
+    const [firstPosition, secondPosition] = positions.split('-');
+
+    const passwordInPieces = password.split('');
+
+    const firstPositionIsTrue = passwordInPieces[parseInt(firstPosition) - 1] === passwordShouldContain 
+    const secondPositionIsTrue = passwordInPieces[parseInt(secondPosition) - 1] === passwordShouldContain 
+
+    if((firstPositionIsTrue || secondPositionIsTrue )&& 
+        firstPositionIsTrue !== secondPositionIsTrue
+      ) {
+        validPasswords += 1;
+      }
+  })
+
+  return validPasswords;
+}
+
+
+export default firstPuzzle;
