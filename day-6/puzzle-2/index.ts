5d9bfe0bfacb299998d910dbfccee8bde3146ef5
Daniel Huizenga <djd.huizenga@gmail.com>
2020-12-06 11:52:41 +0100

    Day 6 part 2 complete

--- day-6/puzzle-2/index.ts
+++ day-6/puzzle-2/index.ts
@@ -1,7 +1,23 @@
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
 const secondPuzzle = (
-
+  input: string[]
 ) => {
+  let amountOfQuestionAnsweredWithYes = 0;
+  
+  for (let index = 0; index < input.length; index++) {
+    const groupAnswers = input[index].split(/(?:\r\n|\r|\n)/g).filter(r => r !== '');
+  
+    const allUniqueQuestionsInGroup = [...new Set(groupAnswers.join(''))]
 
+    allUniqueQuestionsInGroup.forEach(question => {
+      const hasQuestionBeenAnsweredByAllInGroup = groupAnswers.every(personsAnswers => personsAnswers.indexOf(question) > -1);
+
+      if (hasQuestionBeenAnsweredByAllInGroup) amountOfQuestionAnsweredWithYes += 1
+    })
+  }
+  
+  return amountOfQuestionAnsweredWithYes
 }
 
 export default secondPuzzle;
